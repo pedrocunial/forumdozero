4707c6255c08fa689486fa1891cbe89232026724
pedrocunial <pedro_cunial@hotmail.com>
2016-09-04 21:37:58 -0300

    Criação de db simples

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, render_template
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-# db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    post_title = db.Column(db.String(140), unique=True)
+    post_text = db.Column(db.String(2000), unique=True)
+    score = db.Column(db.Integer, unique=True)
+
+    def __init__(self, email, post_title, post_text, score):
+        self.email = email
+        self.post_title = post_title
+        self.post_text = post_text
+        self.score = score
 
 
 @app.route("/")
@@ -27,10 +41,29 @@ def social():
 @app.route("/topic", methods=["GET", "POST"])
 def topic():
     if request.method == "POST":
+        email = "johndoe"
+        post_title = request.form["title"]
+        post_text = request.form["msg"]
+        score = 1
+        user = User(email, post_title, post_text, score)
+        db.session.add(user)
+        db.session.commit()
         return "Sua mensagem: {}, sob o título de: {}"\
-            .format(request.form["msg"], request.form["title"])
+            .format(post_text, post_title)
     else:
         return render_template("topic.html")
+
+
+@app.route("/read/<email>")
+def read_email(email):
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return user.email + " Título do post: &lt;" + user.post_title + "&gt;"
+    else:
+        return "Usuário não encontrado", 404
+
+
+db.create_all()
 
 if __name__ == "__main__":
     app.run()
