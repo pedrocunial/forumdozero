e99a9b3c4f03ef97fa95a0f45f4db848e03745ce
Pedro Cunial <pedrocunial@gmail.com>
2016-09-06 14:04:57 -0300

    FEAT: Comentando código e remodelando db

--- app.py
+++ app.py
@@ -3,13 +3,54 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-
+# Iniciando Flask
 app = Flask(__name__)
+# Iniciando SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
 
+class TopicPost(db.Model):
+    """
+    Define os padrões da postagem em um tópico
+    """
+    __tablename__ = "topic_post"
+    # Chave da tabela
+    id = db.Column(db.Integer, primary_key=True)
+    # Email do usuario autor do post
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    # Titulo do post (pode nao ser preenchido)
+    title = db.Column(db.String(140), unique=False, nullable=True)
+    # Conteudo do post
+    text = db.Column(db.String(2000), unique=False, nullable=False)
+    # Pontuacao do post
+    score = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __init__(self, email, title, text):
+        """
+        Construtor
+        """
+        self.email = email
+        self.title = title
+        self.text = text
+        self.score = 1
+
+    def __repr__(self):
+        """
+        Representacao do objeto quando for chamado, tratando a possivel
+        nao existencia de um titulo
+        """
+        if self.title is not None:
+            return "<{}; {}>".format(self.email, self.title)
+        else:
+            return "<{}; No title".format(self.email)
+
+
 class User(db.Model):
+    """
+    Deprecated
+    """
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     post_title = db.Column(db.String(140), unique=True)
@@ -21,6 +62,9 @@ class User(db.Model):
         self.post_title = post_title
         self.post_text = post_text
         self.score = score
+
+    def __repr__(self):
+        return "<{}; {}>".format(self.email, self.post_title)
 
 
 @app.route("/")
